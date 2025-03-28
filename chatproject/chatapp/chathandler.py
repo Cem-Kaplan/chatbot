4f008de41534927547bbc66e9bd2438b0ec4dc9e
Cem Kaplan <cemk46908@gmail.com>
2025-03-28 22:17:34 +0100

    chathandler hinzugefügt, index bearbeitet

---
+++ chatproject/chatapp/chathandler.py
@@ -0,0 +1,12 @@
+"""
+print("Was möchten sie wissen?\n")
+
+
+while True:
+    prompt1 = input()
+        
+    if prompt1 == "Wie heißt du":
+            print("ich bin der chatbot! entwickelt von C. Kaplan")
+    elif prompt1 == "":
+        print("irgendwas schreiben")
+"""
